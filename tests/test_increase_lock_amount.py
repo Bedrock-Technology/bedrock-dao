f9294f4e086c28e2f3160d7d05a044ce1618fd53
Aayush Gautam <aayush@bedrock.technology>
2023-10-27 11:37:27 +0800

    test: add revert path to createLock, increaseLockAmount, increaseLockLength and Withdraw

--- tests/test_increase_lock_amount.py
+++ tests/test_increase_lock_amount.py
@@ -1,9 +1,9 @@
-import pytest
 import math
-from brownie import *
+import brownie
+from brownie import accounts, chain
 
 """
-Test inrease lock amount
+Test inrease lock amount - happy path
 """
 def test_increase_lock_amount__happy_path(setup_contracts, owner, floorToWeek, daysInSeconds):
     
@@ -16,8 +16,9 @@ def test_increase_lock_amount__happy_path(setup_contracts, owner, floorToWeek, d
     
     lockEnd = floorToWeek(chain.time() + daysInSeconds(365))
 
-    ve.createLock(amount/2, lockEnd, {"from": account})
+    tx = ve.createLock(amount/2, lockEnd, {"from": account})
     
+    assert "Locked" in tx.events
     assert token.balanceOf(account) == amount/2
     assert ve.locked(account)[0] == amount/2 # locked amount
     assert ve.locked(account)[1] == lockEnd # lock end
@@ -29,9 +30,36 @@ def test_increase_lock_amount__happy_path(setup_contracts, owner, floorToWeek, d
 
     ve.increaseLockAmount(amount/2, {"from": account})
 
+    assert "Locked" in tx.events
     assert token.balanceOf(account) == 0
     assert ve.locked(account)[0] == amount
     assert ve.locked(account)[1] == lockEnd
 
     _, _, ts = ve.getLastUserPoint(account)
     assert ve.balanceOf(account) == math.floor((amount)/daysInSeconds(365)) * (ve.lockEnd(account) - ts)
+
+"""
+Test inrease lock amount - revert path
+"""
+def test_increase_lock_amount__revert_path(setup_contracts, owner, floorToWeek, daysInSeconds):
+    
+    token, ve, _ = setup_contracts
+    amount = 100e18
+    
+    account = accounts[2]
+    token.mint(account, amount, {"from": owner})
+    token.approve(ve, amount, {"from": account})
+
+    with brownie.reverts("Must stake non zero amount"):
+        ve.increaseLockAmount(0, {"from": account}) # value cannot be zero
+
+    with brownie.reverts("No existing lock found"):
+        ve.increaseLockAmount(amount, {"from": account}) # no existing lock
+
+    tx = ve.createLock(amount, floorToWeek(chain.time() + daysInSeconds(365)), {"from": account})
+    assert "Locked" in tx.events
+
+    chain.sleep(daysInSeconds(365))
+
+    with brownie.reverts("Cannot add to expired lock. Withdraw"):
+        ve.increaseLockAmount(amount, {"from": account}) # lock end has passed
--- tests/test_increase_lock_length.py
+++ tests/test_increase_lock_length.py
@@ -1,6 +1,6 @@
-import pytest
 import math
-from brownie import *
+import brownie
+from brownie import accounts, chain
 
 """
 Test increase lock length
@@ -35,3 +35,39 @@ def test_increase_lock_length__happy_path(setup_contracts, owner, floorToWeek, d
 
     _, _, ts = ve.getLastUserPoint(account)
     assert ve.balanceOf(account) == math.floor((amount)/daysInSeconds(365)) * (ve.lockEnd(account) - ts)
+
+"""
+Test inrease lock length - revert path
+"""
+def test_increase_lock_length__revert_path(setup_contracts, owner, floorToWeek, daysInSeconds):
+    
+    token, ve, _ = setup_contracts
+    amount = 100e18
+    
+    account = accounts[2]
+    token.mint(account, amount, {"from": owner})
+    token.approve(ve, amount, {"from": account})
+
+    with brownie.reverts("Nothing is locked"):
+        ve.increaseLockLength(floorToWeek(chain.time() + daysInSeconds(1)), {"from": account})
+    
+    tx = ve.createLock(amount, floorToWeek(chain.time() + daysInSeconds(7)), {"from": account})
+    assert "Locked" in tx.events
+
+    chain.sleep(daysInSeconds(8))
+
+    with brownie.reverts("Lock expired"):
+        ve.increaseLockLength(floorToWeek(chain.time() + daysInSeconds(1)), {"from": account})
+
+    tx = ve.withdraw({'from': account})
+    assert "Unlocked" in tx.events
+
+    token.approve(ve, amount, {"from": account})
+    tx = ve.createLock(amount, floorToWeek(chain.time() + daysInSeconds(14)), {"from": account})
+    assert "Locked" in tx.events
+    
+    with brownie.reverts("Can only increase lock WEEK"):
+        ve.increaseLockLength(floorToWeek(chain.time() + daysInSeconds(1)), {"from": account})
+    
+    with brownie.reverts("Exceeds maxtime"):
+        ve.increaseLockLength(floorToWeek(chain.time() + daysInSeconds(365 + 7)), {"from": account})
