1630f65a2862c251bbb75f3876b4c91dc1408967
Aayush Gautam <aayush@bedrock.technology>
2023-12-15 17:39:39 +0800

    feat: add upgrade scripts for contracts

---
+++ scripts/upgrade/contracts.py
@@ -0,0 +1,20 @@
+dep_contracts = {
+    "holesky": {
+        "token":   "0x91493342f273BCDc28a4f2BE4b0BF7Bd1c334c20",
+        "ve":      "0x19A84Cb4f25b95990F0B25b15694349Ee1cCc282",
+        "rewards": "0x907982092cb6B75b6d96B4AfF0814bd58A3A56eA",
+        "gauge":   "0x4A06579Dcb6ec1Db2B09388d1f3418Cd78411c77",
+        "gauge1":  "0xbBE67eF19acdC1a2e5Bc5befee6D137150D2961A",
+        "gauge2":  "0x5e05A1d75757e15aB049f25E1Fbf1C28A56814eF",
+        "gauge3":  "0x9Fd8e7AF11Fb82b5efF2227d50E50335bfb22E7A",
+    }, 
+    "goerli": {
+        "token":   "0x07881e8Ac6cAF3C3082227218E94b3D7ffE201fD",
+        "ve":      "0x19Cd39eC64d3AbbcF9A448175400cDDbA97B9f56",
+        "rewards": "0xFEB5317b7cAA5d88929f60a5fe8481cFa9854f9e",
+        "gauge":   "0xa64B2dFfA8818E4A931e7B25ABDDB4CeA12777B4",
+        "gauge1":  "0xc5b76842D6A9CFD61175eB1520Ccf300c8BE8DA0",
+        "gauge2":  "0xBfb0E7eC0B232ed1B5b386D894285b1865C5D6c8",
+        "gauge3":  "0x427706b8ef4349ECb9A1e3ad3C62d7Cc4e11A621",
+    }
+}
