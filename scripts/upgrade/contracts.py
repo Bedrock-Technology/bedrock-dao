4acb5ced7922961b3e4716b9a3c7bb16876a2894
Aayush Gautam <aayush@bedrock.technology>
2024-01-22 16:27:09 +0800

    deploy: add upgrade script for cashier contract

--- scripts/upgrade/contracts.py
+++ scripts/upgrade/contracts.py
@@ -10,5 +10,6 @@ dep_contracts = {
         "ve":      "0x19Cd39eC64d3AbbcF9A448175400cDDbA97B9f56",
         "rewards": "0xFEB5317b7cAA5d88929f60a5fe8481cFa9854f9e",
         "gauge":   "0xa64B2dFfA8818E4A931e7B25ABDDB4CeA12777B4",
+        "cashier": "0xC727b69476359ee3B9fecD3d82b0cB8ef01BC7FC",
     }
 }
