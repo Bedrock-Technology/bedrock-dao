761f4a394c3dd67b0c48f8c12cdde6e560ee73a2
Trisome-Bedrock <trisome@bedrock-technology>
2024-04-08 15:50:28 +0800

    test_token: test burnFrom (Issue #55)

--- tests/test_token.py
+++ tests/test_token.py
@@ -114,7 +114,7 @@ def test_transfer(setup_contracts, owner, zero_address):
     with brownie.reverts("ERC20: transfer to the zero address"):
         token.transfer(zero_address, amount, {'from': owner})
 
-    # Scenario 3: Cannot transfer an amount exceeding the balance.
+    # Scenario 3: Can't transfer an amount exceeding the balance.
     with brownie.reverts("ERC20: transfer amount exceeds balance"):
         token.transfer(lp, amount, {'from': owner})
 
@@ -135,7 +135,7 @@ def test_transfer(setup_contracts, owner, zero_address):
     assert token.balanceOf(lp) == amount
 
 
-def test_transferFrom(setup_contracts, owner, approved_account, zero_address):
+def test_transferFrom(setup_contracts, owner, approved_account):
     token = setup_contracts[0]
 
     amount = 1e18
@@ -146,7 +146,7 @@ def test_transferFrom(setup_contracts, owner, approved_account, zero_address):
     with brownie.reverts("ERC20: insufficient allowance"):
         token.transferFrom(approved_account, owner, amount, {'from': lp})
 
-    # Scenario 3: Cannot transfer an amount exceeding the balance.
+    # Scenario 3: Can't transfer an amount exceeding the balance.
     token.approve(lp, amount, {'from': approved_account})
 
     with brownie.reverts("ERC20: transfer amount exceeds balance"):
@@ -181,7 +181,7 @@ def test_burn(setup_contracts, owner, zero_address):
     with brownie.reverts("ERC20: burn from the zero address"):
         token.burn(amount, {'from': zero_address})
 
-    # Scenario 2: Cannot transfer an amount exceeding the balance.
+    # Scenario 2: Can't burn an amount exceeding the balance.
     with brownie.reverts("ERC20: burn amount exceeds balance"):
         token.burn(amount, {'from': lp})
 
@@ -199,6 +199,38 @@ def test_burn(setup_contracts, owner, zero_address):
 
     tx = token.burn(amount, {'from': lp})
     assert "Transfer" in tx.events
-    assert token.balanceOf(lp) == 0
     assert token.totalSupply() == 0
 
+
+def test_burnFrom(setup_contracts, owner, approved_account):
+    token = setup_contracts[0]
+
+    amount = 1e18
+
+    lp = accounts[2]
+
+    # Scenario 0: Can't burn for insufficient allowance.
+    with brownie.reverts("ERC20: insufficient allowance"):
+        token.burnFrom(approved_account, amount, {'from': lp})
+
+    # Scenario 1: Can't burn an amount exceeding the balance.
+    token.approve(lp, amount, {'from': approved_account})
+
+    with brownie.reverts("ERC20: burn amount exceeds balance"):
+        token.burnFrom(approved_account, amount, {'from': lp})
+
+    # Scenario 2: Can't burn when the contract is paused.
+    token.pause({"from": owner})
+    assert token.paused()
+    with brownie.reverts("Pausable: paused"):
+        token.burnFrom(lp, amount, {'from': lp})
+
+    # Scenario 3: The burn was successful, and the allowance has been updated.
+    token.unpause({"from": owner})
+    assert not token.paused()
+
+    token.mint(approved_account, amount, {'from': owner})
+
+    tx = token.burnFrom(approved_account, amount, {'from': lp})
+    assert "Transfer" in tx.events
+    assert token.totalSupply() == 0
