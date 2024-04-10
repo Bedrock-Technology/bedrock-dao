ca8c2c51ee11da87dd6959ccfb25520121fde52d
Trisome-Bedrock <trisome@bedrock-technology>
2024-04-10 16:01:04 +0800

    test_gauge_controller: draft test_changeGaugeBaseWeight (Issue #56)

--- tests/test_gauge_controller.py
+++ tests/test_gauge_controller.py
@@ -4,14 +4,52 @@ import time
 
 from tests.utils import get_week, estimatedVotingPower
 
+
+# TODO: To be optimized
+def test_changeGaugeBaseWeight(setup_contracts, owner, zero_address):
+    token, gauge_controller = setup_contracts[0], setup_contracts[2]
+
+    w0 = 100*1e18
+
+    gauges = gauge_controller.getGaugeList()
+    assert gauge_controller.getGaugeWeight(gauges[0]) == 0
+    assert gauge_controller.getGaugeWeight(gauges[0], get_week(1)) == 100*1e18
+    w0_old = gauge_controller.gaugeData(gauges[0])[2]
+    assert w0_old == 100*1e18
+    assert gauge_controller.getTypeWeight(0) == 0
+    assert gauge_controller.getTypeWeight(0, get_week(1)) == 1
+    assert gauge_controller.getWeightsSumPerType(0) == 0
+    assert gauge_controller.getWeightsSumPerType(0, get_week(1)) == 100*1e18
+    assert gauge_controller.getTotalWeight() == 0
+    assert gauge_controller.getTotalWeight(get_week(1)) == 100*1e18
+    assert gauge_controller.zero() == 1*1e18
+
+    fake_gauge = accounts[3]
+    oracle = accounts[4]
+
+    # Scenario 1: Only an administrator can change base weight.
+    with brownie.reverts():
+        gauge_controller.changeGaugeBaseWeight(fake_gauge, w0, {'from': oracle})
+
+    # Scenario 2: Can't change the base weight for a gauge that hasn't been added yet.
+    with brownie.reverts("Gauge not added"):
+        gauge_controller.changeGaugeBaseWeight(fake_gauge, w0, {'from': owner})
+
+    # Scenario 3: Base weight will be scheduled for next week.
+    tx = gauge_controller.changeGaugeBaseWeight(gauges[0], w0, {'from': owner})
+    assert "GaugeBaseWeightUpdated" in tx.events
+
+    # Scenario 4: The historical data of base weight, gauge weight, sum weight, type weight, and total weight will be recorded. #  TODO: 100 break
+
+
 """
 Test vote for gauge weight - happy path
 """
 def test_vote_for_gauge_weight__happy_path(setup_contracts, owner, floorToWeek, daysInSeconds):
-    
+
     token, ve, gauge = setup_contracts[0], setup_contracts[1], setup_contracts[2]
     amount = 100e18
-    
+
     voter1 = accounts[2]
     voter2 = accounts[3]
     voters = [voter1,voter2]
@@ -44,10 +82,10 @@ def test_vote_for_gauge_weight__happy_path(setup_contracts, owner, floorToWeek, 
         token.mint(voter, amount, {"from": owner})
         token.approve(ve, amount, {"from": voter})
         ve.createLock(amount,lockEnd, {"from": voter})
-    
+
     _, slope, ts = ve.getLastUserPoint(voter1)
     assert ve.balanceOf(voter1, ts) == estimatedVotingPower(amount, ve.lockEnd(voter1)-ts)
-    
+
     tx = gauge.voteForGaugeWeight(lp_gauge1, 5000, {'from': voter1})
     assert "GaugeVoted" in tx.events
 
