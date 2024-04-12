fd06be3874bda890539a46b72952f4fe2d914aca
Trisome-Bedrock <trisome@bedrock-technology>
2024-04-12 19:02:44 +0800

    cashier, gauge_controller: remove gaugeRelativeWeightWrite function (Issue #66)

--- interfaces/IGaugeController.sol
+++ interfaces/IGaugeController.sol
@@ -15,20 +15,6 @@ interface IGaugeController {
     /// @notice checkpoints gauge weight for missing weeks
     function checkpointGauge(address _gAddr) external;
 
-    /// @notice Get gauge weight normalized to 1e18 and also fill all the unfilled
-    ///         values for type and gauge records
-    /// @dev Any address can call, however nothing is recorded if the values are filled already
-    /// @param _gAddr Gauge address
-    /// @param _time Relative weight at the specified timestamp in the past or present
-    /// @return Value of relative weight normalized to 1e18
-    function gaugeRelativeWeightWrite(address _gAddr, uint256 _time)
-        external
-        returns (uint256);
-
-    function gaugeRelativeWeightWrite(address _gAddr)
-        external
-        returns (uint256);
-
     /// @notice gets the number of gauge registered with the controller.
     function nGauges() external view returns (uint256);
 
