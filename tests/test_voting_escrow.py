6e1fb24b78c2cd2748580b3243baadc418e5f6a5
Trisome-Bedrock <trisome@bedrock-technology>
2024-04-17 12:18:44 +0800

    test_voting_escrow: test totalSupply with timestamp (Issue #56)

--- tests/test_voting_escrow.py
+++ tests/test_voting_escrow.py
@@ -256,25 +256,6 @@ from tests.utils import get_week, estimatedVotingPower
 #
 #     with brownie.reverts("Must have something to withdraw"):
 #         ve.withdraw({"from": account})
-#
-# # TODO: To be optimized
-# """
-# Test balanceOf (at timestamp)
-# """
-# def test_balance_of_with_timestamp__happy_path(setup_contracts, owner, floorToWeek, daysInSeconds):
-#
-#     token, ve = setup_contracts[0], setup_contracts[1]
-#     amount = 100e18
-#
-#     account = accounts[2]
-#     token.mint(account, amount, {"from": owner})
-#     token.approve(ve, amount, {"from": account})
-#
-#     lockEnd = floorToWeek(chain.time() + daysInSeconds(365)) # 1yr lock
-#     ve.createLock(amount, lockEnd, {"from": account})
-#
-#     _, _, ts = ve.getLastUserPoint(account)
-#     assert ve.balanceOf(account, ts) == math.floor(amount/daysInSeconds(4*365)) * (ve.lockEnd(account) - ts)
 
 
 def test_balanceOf(setup_contracts, owner, users, daysInSeconds):
@@ -371,3 +352,35 @@ def test_totalSupply(setup_contracts, owner, users, daysInSeconds):
         token.mint(users[0], amt, {"from": owner})
 
 
+def test_totalSupply_with_timestamp(setup_contracts, owner, users, daysInSeconds):
+    token, ve = setup_contracts[0], setup_contracts[1]
+
+    week = daysInSeconds(7)
+    amt = ve.MAXTIME()*100e18
+    slope = amt / ve.MAXTIME()
+    lock_end = get_week(3)
+
+    # Scenario 1: The total voting power is zero if users have not locked.
+    assert ve.totalSupply() == 0
+
+    # Scenario 2: The total voting power will decrease linearly over time until it reaches zero.
+    for i in range(2):
+        token.mint(users[i], amt, {"from": owner})
+        token.approve(ve, amt, {"from": users[i]})
+        ve.createLock(amt, lock_end, {"from": users[i]})
+
+    for i in range(2):
+        assert ve.totalSupply(get_week()) == 0
+        assert ve.totalSupply(get_week(1)) == 2*slope*2*week
+        assert ve.totalSupply(get_week(2)) == 2*slope*1*week
+        assert ve.totalSupply(get_week(3)) == 0
+
+    for i in range(10):
+        ts = chain.time()
+        if ts <= lock_end:
+            assert ve.totalSupply(get_week(1))/1e19 == 2 * int(slope * (lock_end - get_week(1)))/1e19
+        else:
+            assert ve.totalSupply() == 0
+
+        chain.sleep(week)
+        token.mint(users[0], amt, {"from": owner})
